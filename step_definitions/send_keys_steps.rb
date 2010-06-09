a7127068e7e3db108f1c352c37cc9863e00fc741
Mark Gandolfo <mark@aussiev8.com.au>
2010-06-09 19:35:15 +1000

    initial commit

---
+++ step_definitions/send_keys_steps.rb
@@ -0,0 +1,12 @@
+## Possible Keys
+# :null, :cancel, :help, :backspace, :tab, :clear:return, :enter, :shift, :left_shift, :control, :left_control :alt, :left_alt, :pause,
+# :escape, :space:page_up, :page_down, :end, :home, :left, :arrow_left, :up:arrow_up, :right, :arrow_right:down, :arrow_down, :insert,
+# :delete, :semicolon, :equals, :numpad0, :numpad1, :numpad2, :numpad3, :numpad4, :numpad5, :numpad6, :numpad7, :numpad8, :numpad9,
+# :multiply:add, :separator, :subtract, :decimal, :divide
+#
+
+# And I send "hello" to "#element"
+# And /^I send (#{allowed_keys.join('|')}) to "([^\"]*)"$/ do |key, element|
+And /^I send (.*) to "(.*)"$/ do |key, element|
+  find(element).send(key)
+end
---
+++ support/send_keys.rb
@@ -0,0 +1,28 @@
+class Capybara::Driver::Selenium < Capybara::Driver::Base
+  class Node < Capybara::Node
+   
+    def send(key)
+      allowed_keys = %q(option, null cancel help backspace tab clearreturn enter shift left_shift control left_control alt left_alt pause escape spacepage_up page_down end home left arrow_left uparrow_up right arrow_rightdown arrow_down insert delete semicolon equals numpad0 numpad1 numpad2 numpad3 numpad4 numpad5 numpad6 numpad7 numpad8 numpad9 multiplyadd separator subtract decimal divide, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12)
+      send_key = []
+
+      if key.match(/\[.*\]/i)
+        key.gsub!(/[\[\]]/,'')
+        key = key.split(',')
+      else
+        key = [key]
+      end
+
+      key.each do |k| 
+        if k.match(/(\'|\")/i)
+          send_key << k.gsub(/(\"|\')/, '')
+        elsif allowed_keys.include?(k)
+          send_key << k.to_sym
+        else
+          send_key << "#{k}"
+        end
+      end
+     
+      node.send_keys(send_key)
+    end
+  end
+end
