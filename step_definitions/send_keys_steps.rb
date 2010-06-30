bea51ef981e7a45dabb503bb95440e0f77ff422f
Mark Gandolfo <mark@aussiev8.com.au>
2010-06-30 10:52:08 +1000

    fixed comment, added a space to ensure confusion is reduced.

--- step_definitions/send_keys_steps.rb
+++ step_definitions/send_keys_steps.rb
@@ -1,8 +1,8 @@
 ## Possible Keys
-# :null, :cancel, :help, :backspace, :tab, :clear:return, :enter, :shift, :left_shift, :control, :left_control :alt, :left_alt, :pause,
+# :null, :cancel, :help, :backspace, :tab, :clear, :return, :enter, :shift, :left_shift, :control, :left_control :alt, :left_alt, :pause,
 # :escape, :space:page_up, :page_down, :end, :home, :left, :arrow_left, :up:arrow_up, :right, :arrow_right:down, :arrow_down, :insert,
 # :delete, :semicolon, :equals, :numpad0, :numpad1, :numpad2, :numpad3, :numpad4, :numpad5, :numpad6, :numpad7, :numpad8, :numpad9,
-# :multiply:add, :separator, :subtract, :decimal, :divide
+# :multiply, :add, :separator, :subtract, :decimal, :divide
 #
 
 # And I send "hello" to "#element"
